83c13cbe223fc64f0d2dc06e144554a06e13bdf8
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-19 14:42:06 +0300

    [fft:Fft,Fft_arrays] Impl

---
+++ lib/fft/fft/Fft.h
@@ -0,0 +1,30 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Module:		FFT
+// Authors:		Miroslav Voinarovsky, Anton Egorov
+// Description:	Fast Fourier Transformtaion is based on the
+//				freeware algorithm written by Miroslav Voinarovsky
+//				with insignificant modifications
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+#ifndef FFT_H
+#define FFT_H
+
+
+class Fft
+{
+public:
+
+	void Init(int pLogN);
+	void TransformDirect(std::vector<float> &pRe, std::vector<float> &pIm);
+	void TransformComplement(std::vector<float> &pRe, std::vector<float> &pIm);
+
+private:
+
+	int m_logN;
+	unsigned int m_nMax;
+	std::vector<float> m_wStore_re;
+	std::vector<float> m_wStore_im;
+};
+
+
+#endif // FFT_H
