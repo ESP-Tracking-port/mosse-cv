268cb65c0c37bdfe4b120969f7c44073c338f554
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-30 16:08:48 +0300

    [portsrc:Mem] Impl | Representation flags

--- portsrc/Types/Mem.hpp
+++ portsrc/Types/Mem.hpp
@@ -13,6 +13,30 @@
 namespace Mosse {
 namespace Tp {
 
+static constexpr inline std::uint64_t bit()
+{
+	return 0;
+}
+
+template <class T, class ...Ts>
+static constexpr inline std::uint64_t bit(T t, Ts ...aTs)
+{
+	using List = std::uint64_t[];
+
+	return 1 << t | bit(aTs...);
+}
+
+struct Repr {
+	enum Flag : std::uint64_t {
+		LenU16 = bit(0),
+		LenU32 = bit(1),
+		MaskLen = bit(0, 1),
+		ReprDirect = bit(2),
+		ReprLog2 = bit(3),
+		MaskRepr = bit(2, 3),
+	};
+};
+
 class Geometry {
 	std::size_t elementSizeof;
 };
