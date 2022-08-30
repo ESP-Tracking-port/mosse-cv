d6cc81433b6b57df3bf07a23ce4d44488d6d1294
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-30 13:45:48 +0300

    [portsrc:Ops] Impl | API extensions

--- portsrc/Util/Ops.hpp
+++ portsrc/Util/Ops.hpp
@@ -9,9 +9,49 @@
 #define MOSSE_UTIL_OPS_HPP_
 
 namespace Mosse {
+namespace Tp {
+
+class Image;
+class Roi;
+class PointRowCol;
+
+}  // namespace Tp
+
 namespace Ut {
 
+/// \brief A numeric representation may use fixed point
+///
 class Ops {
+public:
+
+	/// \brief Implementation MUST expect that it will be called multiple times during the tracking process and therefore
+	/// optimize for unnecessary time and memory expenses.
+	///
+	virtual void init(Tp::Roi) = 0;
+	virtual void imageCropInto(Tp::Image aImageReal, void *aBufferComplex) = 0;
+	virtual void imagePreprocess(void *aCropComplex) = 0;
+	virtual void imageConvFftDomain(void *aioImageFft2Complex, void *aMatrixAcomlex, void *aMatrixBcomplex);
+	virtual void fft2(void *aBufferComplex) = 0;
+	virtual void ifft2(void *aBufferComplex) = 0;
+
+	/// \brief Calculates max. value in a matrix.
+	///
+	/// \arg sum (optional) - stores sum of all the values. The result can be used later in `calcPsr`
+	///
+	virtual void maxReal(void *aComplexBuffer, Tp::PointRowCol &aPeakPos, float *sum = nullptr);
+	float calcPsr(void *aComplexBuffer, Tp::PointRowCol aPeak, float *sumHint = nullptr,
+		Tp::PointRowCol aMask = {11, 11});
+	virtual void imageMaxResponse(void *aImageConvolvedFftDomainComplex, aSize) = 0;
+	virtual void imageMaxResponse(void *aImageConvolvedFftDomainComplex, float &aoPsr, aSize);  ///< Same as `imageMaxResponse(void *)`, but also calculates PSR. Optimization, as both operations require linear exploration
+
+	/// \brief Updates the A matrix (ref. to the MOSSE paper:
+	/// https://www.cs.colostate.edu/~draper/papers/bolme_cvpr10.pdf)
+	///
+	/// \arg eta - learning rate. Authors of the paper suggest using `0.125`
+	/// \arg aInitial - if false, a weighted element-wise sum of the current and the previous element will be used.
+	///
+	virtual void mataUpdate(void *aMatAcomplex, float aEta, bool aInitial) = 0;
+	virtual void matbUpdate(void *aMatBcomplex, void *aImageCropFftComplex, float aEta, bool aInitial);
 };
 
 }  // namespace Ut
