08eaa0975e3ee27a77cda02f6bf18a2ddaa5cd56
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-30 11:18:28 +0300

    [portsrc:Port] Impl | Initial implementation, structure of the lib

---
+++ portsrc/Types/Port.hpp
@@ -0,0 +1,29 @@
+//
+// Port.hpp
+//
+// Created on: Aug 30, 2022
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#if !defined(MOSSE_UTIL_PORT_HPP_)
+#define MOSSE_UTIL_PORT_HPP_
+
+namespace Mosse {
+namespace Ut {
+
+class Fft;
+class Ops;
+class Mem;
+
+/// \brief Replaceable operations which take number representation into account
+///
+struct Port {
+	Fft &fft;
+	Ops &ops;
+	Mem &mem;
+};
+
+}  // namespace Ut
+}  // namespace Mosse
+
+#endif // MOSSE_UTIL_PORT_HPP_
