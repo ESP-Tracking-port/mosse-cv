a89cc63d7bda448276e6da19771176dbf038d711
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-30 11:18:28 +0300

    [portsrc:U16Tracker] Impl | Initial implementation, structure of the lib

---
+++ portsrc/U16Tracker.hpp
@@ -0,0 +1,20 @@
+//
+// U16Tracker.hpp
+//
+// Created on: Aug 29, 2022
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#if !defined(MOSSE_U16TRACKER_HPP_)
+#define MOSSE_U16TRACKER_HPP_
+
+#include "Tracker.hpp"
+
+namespace Mosse {
+
+class U16Tracker : public Tracker {
+};
+
+}  // namespace Mosse
+
+#endif // MOSSE_U16TRACKER_HPP_
