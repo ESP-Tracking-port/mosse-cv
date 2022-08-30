549e5f48e0492675fd553848963dc954c19c6a7c
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-30 11:18:28 +0300

    [portsrc:Ops] Impl | Initial implementation, structure of the lib

---
+++ portsrc/Util/Ops.hpp
@@ -0,0 +1,20 @@
+//
+// Ops.hpp
+//
+// Created on: Aug 29, 2022
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#if !defined(MOSSE_UTIL_OPS_HPP_)
+#define MOSSE_UTIL_OPS_HPP_
+
+namespace Mosse {
+namespace Ut {
+
+class Ops {
+};
+
+}  // namespace Ut
+}  // namespace Mosse
+
+#endif // MOSSE_UTIL_OPS_HPP_
