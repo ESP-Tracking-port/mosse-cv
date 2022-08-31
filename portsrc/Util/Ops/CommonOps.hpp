a07894c5cdb11db357264aa949b6ed97bd9a76f5
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-31 18:03:18 +0300

    [Ops:CommonOps] Impl | Basics

---
+++ portsrc/Util/Ops/CommonOps.hpp
@@ -0,0 +1,111 @@
+//
+// CommonOps.hpp
+//
+// Created on: Aug 30, 2022
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#if !defined(MOSSE_UTIL_OPS_COMMONOPS_HPP_)
+#define MOSSE_UTIL_OPS_COMMONOPS_HPP_
+
+#include "Types/Tracking.hpp"
+#include "Types/Repr.hpp"
+#include "Util/Arithm.hpp"
+#include "Util/Ops.hpp"
+#include "Util/EigenVisitor.hpp"
+#include <Eigen/Core>
+#include <type_traits>
+#include <cassert>
+
+namespace Mosse {
+namespace Ut {
+
+template <Tp::Repr::Flags kReprBuffer>
+class CommonOps : public Ops {
+private:
+	template <bool F>
+	using En = typename std::enable_if<F>::type *;
+public:
+	static_assert(kReprBuffer & (Tp::Repr::CplxRe1Im1 | Tp::Repr::CplxRenImn), "");
+
+	/// \brief Initializes the buffer by splitting the image into its real and imaginary (zeroed) part
+	///
+	void imageCropInto(Tp::Image aImageReal, void *aBufferComplex) override
+	{
+		const auto cols = aImageReal.cols;
+		const auto rows = aImageReal.rows;
+		bufferComplexInitRe(aImageReal, aBufferComplex);
+		bufferComplexInitImZeros(aBufferComplex);
+	}
+private:
+	template <Tp::Repr::Flags F>
+	void bufferComplexInitRe(Tp::Image aImage, void *aBufferCplx)
+	{
+	}
+
+	/// \brief Re1 Im1 order, short representation (length is less than that of the CPU's registers)
+	///
+	template <Tp::Repr::Flags F>
+	void bufferComplexInitImZeros(void *aBufferCplx,
+		En<(F & Tp::Repr::CplxRe1Im1) && (szof<F>() < sizeof(std::size_t))> = nullptr)
+	{
+		constexpr auto kReprSizeof = szof<kReprBuffer>();
+		using ValueType = typename Tp::Repr::template Type<F>;
+		constexpr auto maskAb = ~Ut::maskAb<ValueType, kReprSizeof>();
+		const auto signalLength = roi().size.cols() * roi().size.rows();
+		const auto bufferSize = signalLength * kReprSizeof * 2 / sizeof(std::size_t);
+		assert(bufferSize % sizeof(std::size_t) == 0);
+
+		for (std::size_t i = 0; i < bufferSize; i += sizeof(std::size_t)) {
+			*(static_cast<std::size_t *>(aBufferCplx) + i) &= maskAb;
+		}
+	}
+
+	/// \brief Re1 Im1 order, long representation (equal to the length of the CPU's registers)
+	///
+	template <Tp::Repr::Flags F>
+	void bufferComplexInitImZeros(void *aBufferCplx,
+		En<(F & Tp::Repr::CplxRe1Im1) && (szof<F>() == sizeof(std::size_t))> = nullptr)
+	{
+		const auto signalLength = roi().size.cols() * roi().size.rows();
+		const auto bufferSize = signalLength * 2;
+
+		for (std::size_t i = 1; i < bufferSize; i += sizeof(std::size_t)) {
+			*(static_cast<std::size_t *>(aBufferCplx) + i) = 0;
+		}
+	}
+
+	/// \brief Finds the max element
+	///
+	/// \pre (1) It must be raw representation,
+	/// \pre (2) The array elements are expected to be unaligned
+	/// \pre (3) Row-major array is expected (elements are addressed as array[row][col])
+	///
+	template <Tp::Repr::Flags F>
+	void maxReal(const void *aComplexBuffer, Tp::PointRowCol &aPos, float *sum)
+	{
+		static constexpr auto kStrideInner = szof<F>() * 2;
+		const auto strideOuter = kStrideInner * roi().size.cols;
+		using ValueType = Tp::Repr::Type<F>;
+		using MatrixType = typename Eigen::Matrix<ValueType, Eigen::Dynamic, Eigen::Dynamic>;
+		using StrideType = typename Eigen::Stride<kStrideInner, Eigen::Dynamic>;
+		using MapType = Eigen::Map<MatrixType, Eigen::Unaligned, StrideType>;
+		MapType map{aComplexBuffer, roi().size.rows, roi().size.cols, StrideType{kStrideInner, strideOuter}};
+
+		if (nullptr == sum) {
+			MaxVisitor<ValueType, F> visitor;
+			map.visit(visitor);
+			aPos = visitor.pos;
+		} else {
+			CompositeVisitor<MaxVisitor<ValueType, F>, FloatSumVisitor<ValueType, F>> visitor;
+			map.visit(visitor);
+			*sum = visitor.template get<1>().sum;
+			aPos = visitor.template get<0>().pos;
+		}
+	}
+};
+
+}  // namespace Ut
+}  // namespace Mosse
+
+#endif  // MOSSE_UTIL_OPS_COMMONOPS_HPP_
