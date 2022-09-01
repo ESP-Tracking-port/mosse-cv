decc5e1be26f5b8aed83b74cca0bcf94ad58b1c5
DmitryMurashov <dmtr.murashov@gmail.com>
2022-09-01 11:02:30 +0300

    [Types:EigenAux] Impl

---
+++ portsrc/Types/EigenAux.hpp
@@ -0,0 +1,30 @@
+//
+// EigenAux.hpp
+//
+// Created on: Aug 31, 2022
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#if !defined(MOSSE_TYPES_EIGENAUX_HPP_)
+#define MOSSE_TYPES_EIGENAUX_HPP_
+
+#include "Types/Repr.hpp"
+#include "Util/Arithm/Arithm.hpp"
+#include "Util/Arithm/MemLayout.hpp"
+#include <Eigen/Core>
+
+namespace Mosse {
+namespace Tp {
+
+template <Repr::Flags F>
+struct EigenMapType {
+	using ValueType = Tp::Repr::Type<F>;
+	using MatrixType = typename Eigen::Matrix<ValueType, Eigen::Dynamic, Eigen::Dynamic>;
+	using StrideType = typename Eigen::Stride<Ut::strideInner<F>(), Eigen::Dynamic>;
+	using Type = Eigen::Map<MatrixType, Eigen::Unaligned, StrideType>;
+};
+
+}  // namespace Tp
+}  // namespace Mosse
+
+#endif // MOSSE_TYPES_EIGENAUX_HPP_
