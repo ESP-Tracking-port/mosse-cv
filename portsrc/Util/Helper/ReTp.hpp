88b8ed97a5230235701436f14594f4584a88bac4
DmitryMurashov <dmtr.murashov@gmail.com>
2022-09-02 09:42:32 +0300

    [Helper:ReTp] Impl

---
+++ portsrc/Util/Helper/ReTp.hpp
@@ -0,0 +1,24 @@
+//
+// ReTp.hpp
+//
+// Created on: Sep 02, 2022
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#if !defined(MOSSE_UTIL_HELPER_RETP_HPP_)
+#define MOSSE_UTIL_HELPER_RETP_HPP_
+
+#include "Types/Repr.hpp"
+
+namespace Mosse {
+namespace Ut {
+
+/// \brief Shortcut type getter for Ut namespace
+///
+template <Tp::Repr::Flags F>
+using ReTp = typename Tp::Repr::Type<F>;
+
+}  // namespace Ut
+}  // namespace Mosse
+
+#endif // MOSSE_UTIL_HELPER_RETP_HPP_
