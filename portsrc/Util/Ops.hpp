b87527054a089897e5dcc9dc71d4b46892e91af2
DmitryMurashov <dmtr.murashov@gmail.com>
2022-09-02 12:43:26 +0300

    [Util:Ops] Impl | Change of signature

--- portsrc/Util/Ops.hpp
+++ portsrc/Util/Ops.hpp
@@ -40,7 +40,7 @@ public:
 	/// \arg sum (optional) - stores sum of all the values. The result can be used later in `calcPsr`
 	///
 	virtual void maxReal(const void *aComplexBuffer, Tp::PointRowCol &aPeakPos, float *sum = nullptr);
-	float calcPsr(const void *aComplexBuffer, const Tp::PointRowCol &aPeak, float *sumHint = nullptr,
+	float calcPsr(const void *aComplexBuffer, const Tp::PointRowCol &aPeak, float sumHint,
 		Tp::PointRowCol aMask = {11, 11});
 
 	/// \brief Updates the A matrix (ref. to the MOSSE paper:
