6c4c9c36706bb47d63c689128b9d061b3f35c7e8
DmitryMurashov <dmtr.murashov@gmail.com>
2022-09-05 16:28:32 +0300

    [mosseapi:MosseApi] Enh | Updated the lib

--- lib/mosseapi/MosseApi.hpp
+++ lib/mosseapi/MosseApi.hpp
@@ -8,7 +8,7 @@ namespace Mosse {
 const float *getHann(unsigned rows, unsigned cols);
 std::pair<const float *, const float *> getGaussKernelFft(unsigned rows, unsigned cols);
 const float *getGaussKernelFft3d(unsigned rows, unsigned cols);
-const float *getGaussKernelFft3dScaled125(unsigned &aRows, unsigned &aCols);
+const float *getGaussKernelFft3dScaled125(unsigned aRows, unsigned aCols);
 
 void getClosestWindow(unsigned &aRows, unsigned &aCols);
 const float *getLogTable8bit();  ///< Returns a float log table for integers from 1 to 256
