f982e52528c4fdc6c8a16e71aa3b4fe00ffc857f
DmitryMurashov <dmtr.murashov@gmail.com>
2022-09-06 09:48:08 +0300

    [portsrc:Mosse] Impl | API

---
+++ portsrc/Mosse.hpp
@@ -0,0 +1,18 @@
+//
+// Mosse.hpp
+//
+// Created on: Sep 06, 2022
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#if !defined(MOSSE_MOSSE_HPP_)
+#define MOSSE_MOSSE_HPP_
+
+#include "Types/Tracking.hpp"
+#include "Types/Port.hpp"
+#include "Util/Ops/RawF32Ops.hpp"
+#include "Util/Mem/DynRawF32Mem.hpp"
+#include "Tracker.hpp"
+#include "Mosse.hpp.fwd"
+
+#endif // MOSSE_MOSSE_HPP_
---
+++ portsrc/Util/Mosse.cpp
@@ -0,0 +1,22 @@
+//
+// Mosse.cpp
+//
+// Created on: Sep 06, 2022
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#include "Mosse.hpp"
+
+namespace Mosse {
+
+Tracker &getNaive()
+{
+	static Mosse::Ut::RawF32Ops ops;
+	static Mosse::Ut::DynRawF32Mem mem;
+	static Mosse::Ut::Port port{ops, mem};
+	static Mosse::Tracker tracker{port};
+
+	return tracker;
+}
+
+}  // namespace Mosse
