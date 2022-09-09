66520053038ad614f095b932f8558d51e3a19d15
DmitryMurashov <dmtr.murashov@gmail.com>
2022-09-09 17:33:40 +0300

    [portsrc:Mosse] Impl | "Debug stub" tracker variant

--- portsrc/Mosse.cpp
+++ portsrc/Mosse.cpp
@@ -7,6 +7,8 @@
 
 #include "Mosse.hpp"
 #include "MossePort.hpp"
+#include "Util/Ops/OpencvNativeRawF32Ops.hpp"
+#include "Util/Ops/DebugRawF32Ops.hpp"
 
 namespace Mosse {
 
@@ -18,6 +20,14 @@ Tracker &getNaive()
 	static Mosse::Tracker tracker{port};
 
 	return tracker;
+}
+
+Tracker &getDebugStub()
+{
+	static Mosse::Ut::DebugRawF32Ops ops;
+	static Mosse::Ut::DynRawF32Mem mem;
+	static Mosse::Ut::Port port{ops, mem};
+	static Mosse::Tracker tracker{port};
 }
 
 }  // namespace Mosse
