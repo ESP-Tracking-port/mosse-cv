8cdb4048cdda3450dbc9815048fb585e66e88566
DmitryMurashov <dmtr.murashov@gmail.com>
2022-09-09 13:46:00 +0300

    [Ops:DebugRawF32Ops] Impl | Decorator class. WARN. Build failure

---
+++ portsrc/Util/Ops/DebugRawF32Ops.cpp
@@ -0,0 +1,128 @@
+//
+// DebugRawF32Ops.cpp
+//
+// Created on: Sep 09, 2022
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#include "OpencvNativeRawF32Ops.hpp"
+#include "RawF32Ops.hpp"
+#include "DebugRawF32Ops.hpp"
+
+namespace Mosse {
+namespace Ut {
+
+DebugRawF32Ops::DebugRawF32Ops() : wrapped{{}, {}}
+{
+}
+
+void DebugRawF32Ops::imageCropInto(Tp::Image aImageReal, void *aBufferComplex)
+{
+#if 1
+	wrapped.opencvNativeRawF32Ops.imageCropInto(aImageReal, aBufferComplex);
+#else
+	wrapped.rawF32Ops.imageCropInto(aImageReal, aBufferComplex);
+#endif
+}
+
+void DebugRawF32Ops::imagePreprocess(void *aCropComplex)
+{
+#if 1
+	wrapped.opencvNativeRawF32Ops.imagePreprocess(aCropComplex);
+#else
+	wrapped.rawF32Ops.imagePreprocess(aCropComplex);
+#endif
+}
+
+void DebugRawF32Ops::imageConvFftDomain(void *aioCropFft2Complex, void *aMatrixAcomlex, void *aMatrixBcomplex)
+{
+#if 1
+	wrapped.opencvNativeRawF32Ops.imageConvFftDomain(aioCropFft2Complex, aMatrixAcomlex, aMatrixBcomplex);
+#else
+	wrapped.rawF32Ops.imageConvFftDomain(aioCropFft2Complex, aMatrixAcomlex, aMatrixBcomplex);
+#endif
+}
+
+void DebugRawF32Ops::fft2(void *aBufferComplex)
+{
+#if 1
+	wrapped.opencvNativeRawF32Ops.fft2(aBufferComplex);
+#else
+	wrapped.rawF32Ops.fft2(aBufferComplex);
+#endif
+}
+
+void DebugRawF32Ops::ifft2(void *aBufferComplex)
+{
+#if 1
+	wrapped.opencvNativeRawF32Ops.ifft2(aBufferComplex);
+#else
+	wrapped.rawF32Ops.ifft2(aBufferComplex);
+#endif
+}
+
+void DebugRawF32Ops::maxReal(const void *aComplexBuffer, Tp::PointRowCol &aPeakPos, float *sum)
+{
+#if 1
+	wrapped.opencvNativeRawF32Ops.maxReal(aBufferComplex, aPeakPos, sum);
+#else
+	wrapped.rawF32Ops.maxReal(aBufferComplex, aPeakPos, sum);
+#endif
+}
+
+float DebugRawF32Ops::calcPsr(const void *aComplexBuffer, const Tp::PointRowCol &aPeak, float sumHint, Tp::PointRowCol aMask)
+{
+#if 1
+	wrapped.opencvNativeRawF32Ops.calcPsr(aComplexBuffer, aPeak, sumHint, aMask);
+#else
+	wrapped.rawF32Ops.calcPsr(aComplexBuffer, aPeak, sumHint, aMask);
+#endif
+}
+
+void DebugRawF32Ops::mataUpdate(void *aMatAcomplex, const void *aImageCropFftComplex, bool aInitial)
+{
+#if 1
+	wrapped.opencvNativeRawF32Ops.mataUpdate(aMatAcomplex, aImageCropFftComplex, aInitial);
+#else
+	wrapped.rawF32Ops.mataUpdate(aMatAcomplex, aImageCropFftComplex, aInitial);
+#endif
+}
+
+void DebugRawF32Ops::matbUpdate(void *aMatBcomplex, const void *aImageCropFftComplex, bool aInitial)
+{
+#if 1
+	wrapped.opencvNativeRawF32Ops.matbUpdate(aMatBcomplex, aImageCropFftComplex, aInitial);
+#else
+	wrapped.rawF32Ops.matbUpdate(aMatBcomplex, aImageCropFftComplex, aInitial);
+#endif
+}
+
+void DebugRawF32Ops::initImpl()
+{
+#if 1
+	wrapped.opencvNativeRawF32Ops.initImpl();
+#else
+	wrapped.rawF32Ops.initImpl();
+#endif
+}
+
+const void *DebugRawF32Ops::hannMatrix()
+{
+#if 1
+	return wrapped.opencvNativeRawF32Ops.hannMatrix();
+#else
+	return wrapped.rawF32Ops.hannMatrix();
+#endif
+}
+
+const void *DebugRawF32Ops::gaussFft()
+{
+#if 1
+	return wrapped.opencvNativeRawF32Ops.gaussFft();
+#else
+	return wrapped.rawF32Ops.gaussFft();
+#endif
+}
+
+}  // namespace Ut
+}  // namespace Mosse
---
+++ portsrc/Util/Ops/DebugRawF32Ops.hpp
@@ -0,0 +1,48 @@
+//
+// DebugRawF32Ops.hpp
+//
+// Created on: Sep 09, 2022
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#include "Util/Ops.hpp"
+
+namespace Mosse {
+namespace Ut {
+
+class OpencvNativeRawF32Ops;
+class RawF32Ops;
+
+/// \brief Decorator class for gradually reimplementing the reference CV-based MOSSE tracking algorithm implementation
+/// w/ the custom one.
+///
+class DebugRawF32Ops : Ops {
+private:
+	/// \brief All the calls from opencvNative... will be gradually rerouted to rawF32...
+	///
+	struct Wrapped {
+		OpencvNativeRawF32Ops opencvNativeRawF32Ops;
+		RawF32Ops rawF32Ops;
+	};
+public:
+	DebugRawF32Ops();
+	void imageCropInto(Tp::Image aImageReal, void *aBufferComplex) override;
+	void imagePreprocess(void *aCropComplex) override;
+	void imageConvFftDomain(void *aioCropFft2Complex, void *aMatrixAcomlex, void *aMatrixBcomplex) override;
+	void fft2(void *aBufferComplex) override;
+	void ifft2(void *aBufferComplex) override;
+	void maxReal(const void *aComplexBuffer, Tp::PointRowCol &aPeakPos, float *sum = nullptr) override;
+	float calcPsr(const void *aComplexBuffer, const Tp::PointRowCol &aPeak, float sumHint,
+		Tp::PointRowCol aMask = {11, 11}) override;
+	void mataUpdate(void *aMatAcomplex, const void *aImageCropFftComplex, bool aInitial) override;
+	void matbUpdate(void *aMatBcomplex, const void *aImageCropFftComplex, bool aInitial) override;
+protected:
+	void initImpl() override;
+	const void *hannMatrix() override;  ///< Precompiled hann matrix
+	const void *gaussFft() override;  ///< Fouried-transformed precompiled Gaussian matrix
+private:
+	Wrapped wrapped;
+};
+
+}  // namespace Ut
+}  // namespace Mosse
