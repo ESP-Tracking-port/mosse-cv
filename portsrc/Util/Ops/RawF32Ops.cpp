1788b355775f2a156830619231190a6adae32002
DmitryMurashov <dmtr.murashov@gmail.com>
2022-09-14 10:20:18 +0300

    [Ops:RawF32Ops] Ref | Golfed-out the code for debug output

--- portsrc/Util/Ops/RawF32Ops.cpp
+++ portsrc/Util/Ops/RawF32Ops.cpp
@@ -96,16 +96,9 @@ void RawF32Ops::fft2Common(void *aBufferComplex, bool aFwd)
 
 	const int sizes[2] = {roi().size(0), roi().size(1)};
 	auto reim = cv::Mat(2, sizes, CV_32FC2, aBufferComplex);
-
-	ohdebugsecteveryn(RawF32Ops::fft2Common, -1, {
-		ohdebug(RawF32Ops::fft2common, aFwd, "before", reim);
-	});
-
+	ohdebugeveryn(RawF32Ops::fft2Common, -1, aFwd, "before", reim);
 	cv::dft(reim, reim, !aFwd ? (cv::DFT_SCALE | cv::DFT_INVERSE) : 0);
-
-	ohdebugsecteveryn(RawF32Ops::fft2Common, -1, {
-		ohdebug(RawF32Ops::fft2common, aFwd, "after", reim);
-	});
+	ohdebugeveryn(RawF32Ops::fft2Common, -1, aFwd, "after", reim);
 }
 #else
 void RawF32Ops::fft2Common(void *, bool)
