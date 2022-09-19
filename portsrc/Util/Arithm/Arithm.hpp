39ee170843ce2d322041603867424c09a516b46c
DmitryMurashov <dmtr.murashov@gmail.com>
2022-09-19 17:16:34 +0300

    [Arithm:Arithm] Impl | Summation

--- portsrc/Util/Arithm/Arithm.hpp
+++ portsrc/Util/Arithm/Arithm.hpp
@@ -198,6 +198,7 @@ inline bool gt(ReTp<F1> aLhs, ReTp<F2>(aRhs))
 ///
 struct ArithmBase {
 	virtual bool gt(const Tp::NumVariant aLhs, const Tp::NumVariant aRhs) = 0;
+	virtual void sum(const Tp::NumVariant aLhs, const Tp::NumVariant aRhs, Tp::NumVariant &aOut) = 0;
 };
 
 template <Tp::Repr::Flags F>
@@ -205,6 +206,13 @@ struct Arithm : ArithmBase {
 	bool gt(const Tp::NumVariant aLhs, const Tp::NumVariant aRhs) override
 	{
 		return gt<F>(aLhs, aRhs);
+	}
+
+	void sumA3(const Tp::NumVariant aLhs, const Tp::NumVariant aRhs, Tp::NumVariant &aOut) override
+	{
+		ReTp<F> res;
+		sumA3(aLhs, aRhs, res);
+		aOut = {res};
 	}
 };
 
