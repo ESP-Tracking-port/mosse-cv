00d9e2e5bb3758640cd1f58e9bd2876e85812df8
DmitryMurashov <dmtr.murashov@gmail.com>
2022-09-19 14:16:26 +0300

    [test:TestApply] Impl | Testing `ThreadedOps`-related parts

---
+++ test/TestApply.cpp
@@ -0,0 +1,24 @@
+//
+// TestApply.cpp
+//
+// Created on: Sep 19, 2022
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#include <Fft.h>
+#include <Port/MossePort.hpp>
+#include <Types/Tracking.hpp>
+#include <Types/Repr.hpp>
+#include <Util/Ops/ThreadedOps.hpp>
+#include <Util/Ops/RawF32Ops.hpp>
+#include <doctest/doctest.h>
+
+int max(int a, int b)
+{
+	return a > b ? a : b;
+}
+
+TEST_CASE("Apply") {
+	std::tuple<int, int> args{42, 43};
+	std::cout << Mosse::Ut::apply(max, args) << std::endl;
+}
