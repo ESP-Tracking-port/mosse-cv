7d5075c3ff5b67a414c25b7e56159fd10d8413d7
DmitryMurashov <dmtr.murashov@gmail.com>
2022-09-20 11:29:42 +0300

    [Ops:CommonOps] Impl | `calcPsr` components: "at as float", "sum", "abs. dev. sum"

--- portsrc/Util/Ops/CommonOps.hpp
+++ portsrc/Util/Ops/CommonOps.hpp
@@ -138,6 +138,39 @@ public:
 		}
 	}
 
+	float bufferAtAsFloat(const void *aComplexBuffer, const Tp::PointRowCol &aPeak) override
+	{
+		return Ut::fromRepr<float, ReprBuffer>(Ut::atAsVariant<ReprBuffer>(aPeak, roi(), aComplexBuffer));
+	}
+
+	float bufferSum(const void *aComplexBuffer, const Tp::PointRowCol &aPeak, float aSumHint,
+		const Tp::PointRowCol &aMask) override
+	{
+		auto map = Ut::makeEigenMap<ReprBuffer>(aComplexBuffer, roi());
+		Tp::Roi roiMask{roi().origin + aPeak - (aMask / 2), aMask};
+		roiMask.fitShift(roi().size);
+		auto mapMask = map.block(roiMask.origin(0), roiMask.origin(1), roiMask.size(0), roiMask.size(1));
+		FloatSumVisitor<ReprBuffer> visitor;
+		mapMask.visit(visitor);
+		aSumHint -= visitor.sum;
+
+		return aSumHint;
+	}
+
+	virtual float bufferAbsDevSum(const void *aComplexBuffer, const Tp::PointRowCol &aPeak,
+		float aMean, Tp::PointRowCol aMask) override
+	{
+		auto map = Ut::makeEigenMap<ReprBuffer>(aComplexBuffer, roi());
+		auto mapBlock = Ut::makeEigenBlock(map, roiFragment());
+		Tp::Roi roiMask{roi().origin + aPeak - (aMask / 2), aMask};
+		roiMask.fitShift(roi().size);
+		OffsetAdapterVisitor<ReprBuffer, FloatDevSumVisitor<ReprBuffer, true>> visitor{roiFragment().origin,
+			{0.0f, aMean, roiMask}};
+		mapBlock.visit(visitor);
+
+		return visitor.wrapped.devsum;
+	}
+
 	float calcPsr(const void *aComplexBuffer, const Tp::PointRowCol &aPeak, float sumHint,
 		Tp::PointRowCol aMask) override
 	{
