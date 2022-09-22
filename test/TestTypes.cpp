ddac74d4d41e3638f1f3bb8a10f8faaff073c4ed
DmitryMurashov <dmtr.murashov@gmail.com>
2022-09-22 15:22:39 +0300

    [test:TestTypes] Impl | Malloc counting

---
+++ test/TestTypes.cpp
@@ -0,0 +1,23 @@
+//
+// TestTypes.cpp
+//
+// Created on: Sep 22, 2022
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#include <Types/Repr.hpp>
+#include <Mosse.hpp>
+#include <doctest/doctest.h>
+#include <cstdint>
+#include <array>
+#include <OhDebug.hpp>
+#include <MallocCounter.hpp>
+
+TEST_CASE("Test Types : Image copying, memory profiling")
+{
+	std::array<std::uint8_t, 64 * 64> buf {{0}};
+	Mosse::Tp::Image image{buf.data(), 64, 64};
+	MallocCounter counter{};
+	Mosse::Tp::Image *imageCopy = new Mosse::Tp::Image{image};
+	ohdebug("Test Types : Image copying memory profiling", MallocCounter::getPeakCount());
+}
