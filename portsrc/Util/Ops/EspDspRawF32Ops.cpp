9620987bea6f22ea1508a0748c14e955643bb06b
DmitryMurashov <dmtr.murashov@gmail.com>
2022-09-27 17:03:29 +0300

    [Ops:EspDspRawF32Ops] Impl | Debug hooks

--- portsrc/Util/Ops/EspDspRawF32Ops.cpp
+++ portsrc/Util/Ops/EspDspRawF32Ops.cpp
@@ -19,11 +19,38 @@ EspDspRawF32Ops::EspDspRawF32Ops() : RawF32Ops{}, espDspFft2{}
 
 void EspDspRawF32Ops::fft2(void *aBufferComplex)
 {
+	ohdebugsect(EspDspRawF32Ops::fft2, {
+		for (std::size_t row = roiFragment().origin(0); row < roiFragment().size(0); ++row) {
+			for (std::size_t col = roiFragment().origin(1); col < roiFragment().size(1); ++col) {
+				assert(!std::isnan(atAsVariant<reprFlags.buffer>({row, col}, roi(), aBufferComplex).f32));
+				assert(!std::isnan(atImagAsVariant<reprFlags.buffer>({row, col}, roi(), aBufferComplex).f32));
+			}
+		}
+	});
+
 	espDspFft2.fft2(aBufferComplex);
+
+	ohdebugsect(EspDspRawF32Ops::fft2, {
+		for (std::size_t row = roiFragment().origin(0); row < roiFragment().size(0); ++row) {
+			for (std::size_t col = roiFragment().origin(1); col < roiFragment().size(1); ++col) {
+				assert(!std::isnan(atAsVariant<reprFlags.buffer>({row, col}, roi(), aBufferComplex).f32));
+				assert(!std::isnan(atImagAsVariant<reprFlags.buffer>({row, col}, roi(), aBufferComplex).f32));
+			}
+		}
+	});
 }
 
 void EspDspRawF32Ops::ifft2(void *aBufferComplex)
 {
+	ohdebugsect(EspDspRawF32Ops::ifft2, {
+		for (std::size_t row = roiFragment().origin(0); row < roiFragment().size(0); ++row) {
+			for (std::size_t col = roiFragment().origin(1); col < roiFragment().size(1); ++col) {
+				assert(!std::isnan(atAsVariant<reprFlags.buffer>({row, col}, roi(), aBufferComplex).f32));
+				assert(!std::isnan(atImagAsVariant<reprFlags.buffer>({row, col}, roi(), aBufferComplex).f32));
+			}
+		}
+	});
+
 	espDspFft2.ifft2(aBufferComplex);
 }
 
@@ -35,6 +62,20 @@ void EspDspRawF32Ops::initImpl()
 		ohdebug(EspDspRawF32Ops::initImpl);
 		espDspFft2.init(roi(), nullptr, nullptr);
 	}
+}
+
+void EspDspRawF32Ops::imageCropInto(Tp::Image aImageReal, void *aBufferComplex)
+{
+	RawF32Ops::imageCropInto(aImageReal, aBufferComplex);
+
+	ohdebugsect(EspDspRawF32Ops::imageCropInto, {
+		for (std::size_t row = roiFragment().origin(0); row < roiFragment().size(0); ++row) {
+			for (std::size_t col = roiFragment().origin(1); col < roiFragment().size(1); ++col) {
+				assert(!std::isnan(atAsVariant<reprFlags.buffer>({row, col}, roi(), aBufferComplex).f32));
+				assert(!std::isnan(atImagAsVariant<reprFlags.buffer>({row, col}, roi(), aBufferComplex).f32));
+			}
+		}
+	});
 }
 
 }  // namespace Ut
