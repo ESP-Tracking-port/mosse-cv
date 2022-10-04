a0d5eb7888455a38471d571d86d49f399a46b336
DmitryMurashov <dmtr.murashov@gmail.com>
2022-10-04 13:32:29 +0300

    [Port:OsApi] Impl | Integrated OS API into threaded tasks to prevent tasks from running spinlock-checking loop indefinitely

---
+++ portsrc/Port/OsApi.cpp
@@ -0,0 +1,37 @@
+//
+// OsApi.cpp
+//
+// Created on: Oct 04, 2022
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#include "OsApi.hpp"
+#include "Port/MossePort.hpp"
+
+namespace Mosse {
+namespace Port {
+
+static OsApi *sOsApiInstance = nullptr;
+
+OsApi *OsApi::instance()
+{
+	return sOsApiInstance;
+}
+
+void OsApi::setInstance(OsApi &aInstance)
+{
+	mosse_assert(sOsApiInstance == nullptr);
+	sOsApiInstance = &aInstance;
+}
+
+StubOsApi::StubOsApi()
+{
+	setInstance(*this);
+}
+
+void StubOsApi::taskYieldMinDelay()
+{
+}
+
+}  // namespace Port
+}  // namespace Mosse
---
+++ portsrc/Port/OsApi.hpp
@@ -0,0 +1,44 @@
+//
+// OsApi.hpp
+//
+// Created on: Oct 04, 2022
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#if !defined(MOSSE_PORT_OSAPI_HPP_)
+#define MOSSE_PORT_OSAPI_HPP_
+
+namespace Mosse {
+namespace Port {
+
+class OsApi {
+public:
+	/// \brief Yield the control to another task for the shortest possible period of time, when a current task has nothing
+	/// useful to do apart from waiting for certain conditions to be met.
+	///
+	/// \details Use case: to prevent watchdog triggering in systems like FreeRTOS
+	///
+	virtual void taskYieldMinDelay() = 0;
+
+	static OsApi *instance();
+protected:
+	OsApi() = default;
+	OsApi(const OsApi &) = delete;
+	OsApi(OsApi &&) = delete;
+	OsApi &operator=(const OsApi &) = delete;
+	OsApi &operator=(OsApi &&) = delete;
+	static void setInstance(OsApi &aInstance);
+private:
+	static OsApi *inst;
+};
+
+class StubOsApi : public OsApi {
+public:
+	StubOsApi();
+	void taskYieldMinDelay() override;
+};
+
+}  // namespace Port
+}  // namespace Mosse
+
+#endif // MOSSE_PORT_OSAPI_HPP_
