74d9133a262668176074b6eab3f15115267f7bca
DmitryMurashov <dmtr.murashov@gmail.com>
2022-10-05 10:48:31 +0300

    [Port:OsApi] Impl | Conditional wrap over task delay making it possible to disable yielding control

--- embmosse/Port/OsApi.hpp
+++ embmosse/Port/OsApi.hpp
@@ -12,7 +12,28 @@ namespace Mosse {
 namespace Port {
 
 class OsApi {
+private:
+	struct TaskYield {
+		bool enable;
+	};
 public:
+	/// \brief More fine-tuned (conditional) approach to whether the control should be yielded. Enables to retain the
+	/// control for the duration of frame processing
+	///
+	inline void taskYieldMinDelayWrap()
+	{
+		if (taskYield.enable) {
+			taskYieldMinDelay();
+		}
+	}
+
+	/// \brief Enables / Disables yield
+	///
+	inline void setTaskYieldMinDelayFlag(bool aShouldYield)
+	{
+		taskYield.enable = aShouldYield;
+	}
+
 	/// \brief Yield the control to another task for the shortest possible period of time, when a current task has nothing
 	/// useful to do apart from waiting for certain conditions to be met.
 	///
@@ -30,6 +51,7 @@ protected:
 	static void setInstance(OsApi &aInstance);
 private:
 	static OsApi *inst;
+	TaskYield taskYield = {true};
 };
 
 class StubOsApi : public OsApi {
